4f57ae5dcb91c29726e1e9d436ef914eb2fbb628
mailsanjayhere <mailsanjayhere@gmail.com>
2024-03-13 18:59:59 +0530

    added ref

--- CIS/Cisco_ios/11_local_authentication_authorization_and_accounting_rules/rule_111_enable_aaa_new_model.py
+++ CIS/Cisco_ios/11_local_authentication_authorization_and_accounting_rules/rule_111_enable_aaa_new_model.py
@@ -1,21 +1,9 @@
-from comfy.compliance import medium
-
+from comfy.compliance import *  # noqa
 
 @medium(
     name='rule_111_enable_aaa_new_model',
     platform=['cisco_ios', 'cisco_xe']
 )
-def rule_111_enable_aaa_new_model(configuration):
-    uri = (
-        "http://www.cisco.com/en/US/docs/ios-xml/ios/security/a1/sec-cr-a2.html#GUID-E05C2E00-C01E-40"
-        "53-9D12-EC37C7E8EEC5"
-    )
+def rule_111_enable_aaa_new_model(configuration,ref):
+    assert 'no aaa new-model' not in configuration, ref
 
-    remediation = (f"""
-    Remediation: hostname(config)#aaa new-model
-
-    References: {uri}
-
-    """)
-
-    assert 'no aaa new-model' not in configuration, remediation
