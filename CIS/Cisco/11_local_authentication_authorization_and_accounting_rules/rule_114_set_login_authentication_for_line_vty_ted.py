4543afc02bba562a961ad6dc6bdd22a6f104e338
Netpicker <156186606+netpicker@users.noreply.github.com>
2024-03-20 08:44:57 +0000

    Move Cisco folder to Cisco_ios

--- CIS/Cisco/11_local_authentication_authorization_and_accounting_rules/rule_114_set_login_authentication_for_line_vty_ted.py
+++
@@ -1,9 +0,0 @@
-from comfy.compliance import medium
-
-@medium(
-  name='rule_114_set_login_authentication_for_line_vty_ted',
-  platform=['cisco_ios', 'cisco_xe'],
-  commands=dict(chk_cmd='show running-config | sec line | incl login authentication')
-)
-def rule_114_set_login_authentication_for_line_vty_ted(commands,ref):
-    assert ' login authentication' in commands.chk_cmd, ref
--- CIS/Cisco_ios/11_local_authentication_authorization_and_accounting_rules/rule_1110_set_aaa_accounting_system.py
+++ CIS/Cisco_ios/11_local_authentication_authorization_and_accounting_rules/rule_1110_set_aaa_accounting_system.py
@@ -1,22 +1,8 @@
 from comfy.compliance import low
 
-
 @low(
   name='rule_1110_set_aaa_accounting_system',
   platform=['cisco_ios', 'cisco_xe']
 )
-def rule_1110_set_aaa_accounting_system(configuration):
-    uri = (
-        "http://www.cisco.com/en/US/docs/ios-xml/ios/security/a1/sec-cr-a1.html#GUID-0520BCEF-89FB-45"
-        "05-A5DF-D7F1389F1BBA"
-    )
-
-    remediation = (f"""
-    Remediation: hostname(config)#aaa accounting system [[default | list-name | guarantee -
-                 first] [start-stop | stop-only | none] [radius | group group-name]
-
-    References: {uri}
-
-    """)
-
-    assert 'aaa accounting system' in configuration, remediation
+def rule_1110_set_aaa_accounting_system(configuration,ref):
+    assert 'aaa accounting system' in configuration, ref
