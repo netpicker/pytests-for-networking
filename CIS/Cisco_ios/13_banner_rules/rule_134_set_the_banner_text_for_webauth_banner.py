5fa8322182d3a27bd53ef029b049f039e3681569
mailsanjayhere <mailsanjayhere@gmail.com>
2024-04-22 16:19:58 +0530

    Remove trailing whitespaces

---
+++ CIS/Cisco_ios/13_banner_rules/rule_134_set_the_banner_text_for_webauth_banner.py
@@ -0,0 +1,11 @@
+from comfy.compliance import medium
+
+
+@medium(
+    name='rule_134_set_the_banner_text_for_webauth_banner',
+    platform=['cisco_ios', 'cisco_xe'],
+    commands={'chk_cmd': 'show ip admission auth-proxy-banner http'}
+)
+def rule_134_set_the_banner_text_for_webauth_banner(commands, ref):
+    banner_text = commands['chk_cmd']
+    assert 'Unauthorized access is prohibited' in banner_text, ref + " - Missing or incorrect banner text."
