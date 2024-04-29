4fa82b1399455308f1c29a316e86fac668354806
mailsanjayhere <mailsanjayhere@gmail.com>
2024-04-29 18:54:39 +0530

    added in 3315

---
+++ CIS/Cisco_ios/331_require_eigrp_auth_if_used/rule_3315_set_af_interface_default.py
@@ -0,0 +1,14 @@
+from comfy import medium
+
+
+@medium(
+    name='rule_3315_set_af_interface_default',
+    platform=['cisco_ios', 'cisco_xe'],
+    commands={'eigrp_af_config': 'sh run | sec router eigrp'}
+)
+def rule_3315_set_af_interface_default(commands, ref):
+    # Extracting the EIGRP address family interface configuration from the command output
+    eigrp_af_config = commands['eigrp_af_config']
+
+    # Verifying that 'af-interface default' is configured within the EIGRP address family
+    assert 'af-interface default' in eigrp_af_config, ref
