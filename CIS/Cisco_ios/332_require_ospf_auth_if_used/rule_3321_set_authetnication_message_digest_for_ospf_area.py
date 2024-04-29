28d78152e16d13678c24d3d0fb8f057fb39826ee
Netpicker <156186606+netpicker@users.noreply.github.com>
2024-04-29 11:21:39 +0530

    Netpicker adding linting (#41)
    * Add modified service password encryption rule
    
    * modified snmp files
    
    * Remove trailing whitespaces
    
    * Create rule_159_set_priv_for_each_snmp_server_group.py
    
    * Create rule_1510_require_aes_128_as_minimum_for_snmp_server.py
    
    * Create rule_164_configure_web_interface.py
    
    * Create rule_314_set_ip_verify_unicast_source_reachable_via.py
    
    * added 32 border router filtering
    
    * 32 folder, adding
    
    * added new folders (#39)
    
    * Update and rename pylint.yml to flake8.yml (#25)
    
    * Create .flake8 (#26)
    
    * Rename dirs (#27)
    
    * Update .flake8 (#28)
    
    * Update .gitignore (#29)
    
    * Update flake8.yml (#30)
    
    * Add CVEs and formal (#31)
    
    * Add CVE rules
    
    * Save formal config for Cisco XR
    
    * Newlines
    
    * Fix whitespace
    
    * Add display set command for Juniper (#32)
    
    * Add modified service password encryption rule (#33)
    
    Co-authored-by: mailsanjayhere <mailsanjayhere@gmail.com>
    
    * snmp pytests updated (#34)
    
    * Add modified service password encryption rule
    
    * modified snmp files
    
    ---------
    
    Co-authored-by: mailsanjayhere <mailsanjayhere@gmail.com>
    
    * Netpicker adding linting (#35)
    
    * Add modified service password encryption rule
    
    * modified snmp files
    
    * Remove trailing whitespaces
    
    * Create rule_159_set_priv_for_each_snmp_server_group.py
    
    * Create rule_1510_require_aes_128_as_minimum_for_snmp_server.py
    
    ---------
    
    Co-authored-by: mailsanjayhere <mailsanjayhere@gmail.com>
    
    * Create rule_163_configuring_kerberos.py (#36)
    
    * Netpicker adding linting (#37)
    
    * Add modified service password encryption rule
    
    * modified snmp files
    
    * Remove trailing whitespaces
    
    * Create rule_159_set_priv_for_each_snmp_server_group.py
    
    * Create rule_1510_require_aes_128_as_minimum_for_snmp_server.py
    
    * Create rule_164_configure_web_interface.py
    
    * Create rule_314_set_ip_verify_unicast_source_reachable_via.py
    
    ---------
    
    Co-authored-by: mailsanjayhere <mailsanjayhere@gmail.com>
    
    ---------
    
    Co-authored-by: mailsanjayhere <mailsanjayhere@gmail.com>
    
    * 32_border_router_filtering/rule_321_set_ip_access_list_extended_to_forbid_private_source.py
    
    * 3341 added
    
    * adding ospf auth rules
    
    ---------
    
    Co-authored-by: mailsanjayhere <mailsanjayhere@gmail.com>

---
+++ CIS/Cisco_ios/332_require_ospf_auth_if_used/rule_3321_set_authetnication_message_digest_for_ospf_area.py
@@ -0,0 +1,14 @@
+from comfy import medium
+
+
+@medium(
+    name='rule_3321_set_authentication_message_digest_for_ospf_area',
+    platform=['cisco_ios'],
+    commands={'ospf_config': 'sh run | sec router ospf'}
+)
+def rule_3321_set_authentication_message_digest_for_ospf_area(commands, ref):
+    # Extracting the OSPF configuration section from the command output
+    ospf_config = commands['ospf_config']
+
+    # Checking if 'authentication message-digest' is configured in the OSPF section
+    assert 'area' in ospf_config and 'authentication message-digest' in ospf_config, ref
