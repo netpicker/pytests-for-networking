fd26c889cdba5ccaa7d48f9cd0346f895a8d3028
mailsanjayhere <mailsanjayhere@gmail.com>
2024-04-29 12:18:50 +0530

    added rip auth rules

---
+++ CIS/Cisco_ios/333_require_ripv2_auth_if_used/rule_3334_set_ip_rip_authentication_key_chain.py
@@ -0,0 +1,16 @@
+from comfy import medium
+
+
+@medium(
+    name='rule_3334_set_ip_rip_authentication_key_chain',
+    platform=['cisco_ios', 'cisco_xe'],
+    commands={'interface_rip_config': 'sh run int {interface_name}'}
+)
+def rule_3334_set_ip_rip_authentication_key_chain(commands, ref):
+    # Replace {interface_name} and {rip_key-chain_name} with the actual interface and key chain names you want to test.
+
+    # Extracting the RIP v2 authentication configuration from the command output
+    interface_rip_config = commands['interface_rip_config']
+
+    # Verifying that the RIP v2 authentication key chain is properly configured on the interface
+    assert 'ip rip authentication key-chain' in interface_rip_config, ref
---
+++ CIS/Cisco_ios/333_require_ripv2_auth_if_used/rule_3335_set_ip_rip_authentication_mode_to_md5.py
@@ -0,0 +1,16 @@
+from comfy import medium
+
+
+@medium(
+    name='rule_3335_set_ip_rip_authentication_mode_to_md5',
+    platform=['cisco_ios', 'cisco_xe'],
+    commands={'interface_rip_mode_config': 'sh run int {interface_name}'}
+)
+def rule_3335_set_ip_rip_authentication_mode_to_md5(commands, ref):
+    # Replace {interface_name} with the actual interface you want to test.
+
+    # Extracting the RIP v2 MD5 authentication mode configuration from the command output
+    interface_rip_mode_config = commands['interface_rip_mode_config']
+
+    # Verifying that the RIP v2 authentication mode is set to MD5 on the interface
+    assert 'ip rip authentication mode md5' in interface_rip_mode_config, ref
