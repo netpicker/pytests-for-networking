dc9f0047b8e37064d895425ea2d86ced2ea7c8e7
mailsanjayhere <mailsanjayhere@gmail.com>
2024-04-29 18:19:47 +0530

    added back 331

---
+++ CIS/Cisco_ios/331_require_eigrp_auth_if_used/rule_3312_set_key.py
@@ -0,0 +1,14 @@
+from comfy import medium
+
+
+@medium(
+    name='rule_3312_set_key',
+    platform=['cisco_ios', 'cisco_xe'],
+    commands={'key_chain_config': 'sh run | sec key chain'}
+)
+def rule_3332_set_key(commands, ref):
+    # Extracting the key chain configuration from the command output
+    key_chain_config = commands['key_chain_config']
+
+    # Verifying that the key is properly set within a key chain
+    assert 'key' in key_chain_config, ref
