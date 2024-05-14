596b08f01a47271c5b7665d8398e41725abcab71
mailsanjayhere <mailsanjayhere@gmail.com>
2024-05-14 10:43:55 +0530

    updating rule_21

--- CIS/Junos/2_Firewall/rule_2_1_ensure__protect_re__firewall_filter_is_set_for_inbound_traffic_to_the_routing_engine.py
+++ CIS/Junos/2_Firewall/rule_2_1_ensure__protect_re__firewall_filter_is_set_for_inbound_traffic_to_the_routing_engine.py
@@ -1,5 +1,6 @@
 from comfy.compliance import medium
 
+
 @medium(
     name='rule_2_1_ensure__protect_re__firewall_filter_is_set_for_inbound_traffic_to_the_routing_engine',
     platform=['juniper_junos'],
@@ -10,7 +11,7 @@ from comfy.compliance import medium
         'show_lo0_ipv6_filter': 'show interfaces lo0 | display set | match "filter input family inet6"'
     }
 )
-def rule_2_1_ensure__protect_re__firewall_filter_is_set_for_inbound_traffic_to_the_routing_engine(configuration, commands, device, devices):
+def rule_2_1_ensure__protect_re__firewall_filter_is_set_for_inbound_traffic_to_the_routing_engine(commands, ref):
     # Check if IPv4 and IPv6 are configured on the device
     ipv4_configured = int(commands['show_ipv4_interfaces']) > 0
     ipv6_configured = int(commands['show_ipv6_interfaces']) > 0
@@ -18,12 +19,12 @@ def rule_2_1_ensure__protect_re__firewall_filter_is_set_for_inbound_traffic_to_t
     # Check for the presence of the firewall filters on the loopback interface for IPv4
     ipv4_filter_present = 'family inet filter' in commands['show_lo0_ipv4_filter']
     if ipv4_configured:
-        assert ipv4_filter_present, "IPv4 firewall filter missing on lo0 interface"
+        assert ipv4_filter_present, ref
 
     # Check for the presence of the firewall filters on the loopback interface for IPv6
     ipv6_filter_present = 'family inet6 filter' in commands['show_lo0_ipv6_filter']
     if ipv6_configured:
-        assert ipv6_filter_present, "IPv6 firewall filter missing on lo0 interface"
+        assert ipv6_filter_present, ref
 
     # Additional checks can be added here to verify the specifics of the filter rules
     # such as ensuring they only allow management services from trusted hosts
