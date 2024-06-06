839297d9cd20e61cbdede2b2c92bad32a650398a
Netpicker <156186606+netpicker@users.noreply.github.com>
2024-06-06 15:08:32 +0200

    Update rule_122_set_transport_input_ssh_for_line_vty_connections.py (#61)

--- CIS/Cisco_ios/12_access_rules/rule_122_set_transport_input_ssh_for_line_vty_connections.py
+++ CIS/Cisco_ios/12_access_rules/rule_122_set_transport_input_ssh_for_line_vty_connections.py
@@ -7,4 +7,4 @@ from comfy.compliance import medium
     commands=dict(chk_cmd='show running-config | sec vty')
 )
 def rule_122_set_transport_input_ssh_for_line_vty_connections(commands, ref):
-    assert ' transport input ssh' in commands.chk_cmd, ref
+    assert 'transport input ssh' in commands.chk_cmd, ref
