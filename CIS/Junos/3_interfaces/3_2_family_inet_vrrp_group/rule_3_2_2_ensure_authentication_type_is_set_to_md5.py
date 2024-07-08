3108a73b5262748a73ffbf97ed0172bee8a73449
Netpicker <156186606+netpicker@users.noreply.github.com>
2024-07-08 18:13:02 +0530

    reorganized 3_interfaces folder (#71)
    * removing ruleset files
    
    * adding 3_interface rules and refs
    
    * Revert "adding 3_interface rules and refs"
    
    This reverts commit 6ec630d4fdee40918c0277ab73ad0d30fcbe45cf.
    
    * added 3_interfaces rules and refs again
    
    * added 4_protocols tests
    
    * restrctured 3_interfaces folder
    
    ---------
    
    Co-authored-by: mailsanjayhere <mailsanjayhere@gmail.com>

---
+++ CIS/Junos/3_interfaces/3_2_family_inet_vrrp_group/rule_3_2_2_ensure_authentication_type_is_set_to_md5.py
@@ -0,0 +1,10 @@
+from comfy.compliance import low
+
+
+@low(
+      name='rule_3_2_2_ensure_authentication_type_is_set_to_md5',
+      platform=['juniper'],
+      commands=dict(chk_cmd='')
+)
+def rule_3_2_2_ensure_authentication_type_is_set_to_md5(commands, ref):
+    assert '' in commands.chk_cmd, ref
