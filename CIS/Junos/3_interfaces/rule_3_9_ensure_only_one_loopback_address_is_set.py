14cb3006e6367284b394917aa888f29bc1f0c456
mailsanjayhere <mailsanjayhere@gmail.com>
2024-07-08 17:24:40 +0530

    added 3_interfaces rules and refs again

---
+++ CIS/Junos/3_interfaces/rule_3_9_ensure_only_one_loopback_address_is_set.py
@@ -0,0 +1,10 @@
+from comfy.compliance import medium
+
+
+@medium(
+      name='rule_3_9_ensure_only_one_loopback_address_is_set',
+      platform=['juniper'],
+      commands=dict(chk_cmd='')
+)
+def rule_3_9_ensure_only_one_loopback_address_is_set(commands, ref):
+    assert '' in commands.chk_cmd, ref
