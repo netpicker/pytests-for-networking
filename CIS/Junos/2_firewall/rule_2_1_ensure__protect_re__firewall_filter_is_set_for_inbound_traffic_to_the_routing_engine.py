e6587eb5aac6270d60467790ac05df43ad328695
Netpicker <156186606+netpicker@users.noreply.github.com>
2024-07-08 12:11:29 +0530

    junos addition (#63)
    Co-authored-by: mailsanjayhere <mailsanjayhere@gmail.com>

---
+++ CIS/Junos/2_firewall/rule_2_1_ensure__protect_re__firewall_filter_is_set_for_inbound_traffic_to_the_routing_engine.py
@@ -0,0 +1,10 @@
+from comfy.compliance import medium
+
+
+@medium(
+      name='rule_2_1_ensure__protect_re__firewall_filter_is_set_for_inbound_traffic_to_the_routing_engine',
+      platform=['juniper'],
+      commands=dict(chk_cmd='')
+)
+def rule_2_1_ensure__protect_re__firewall_filter_is_set_for_inbound_traffic_to_the_routing_engine(commands, ref):
+    assert '' in commands.chk_cmd, ref
