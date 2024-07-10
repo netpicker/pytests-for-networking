0888ad930aafc9ffe61c29dc2b2f2e0d190a9c99
Netpicker <156186606+netpicker@users.noreply.github.com>
2024-07-10 23:43:54 +0530

    updated rule_1_1 test (#76)
    * updated rule_1_1 test
    
    * fixing flake8 reported errors
    
    * fixing flake8 errors for all vendors
    
    * fixed some more flake8
    
    ---------
    
    Co-authored-by: mailsanjayhere <mailsanjayhere@gmail.com>

--- CIS/Junos/1_general_recommendations/rule_1_1_ensure_device_is_running_current_junos_software.py
+++ CIS/Junos/1_general_recommendations/rule_1_1_ensure_device_is_running_current_junos_software.py
@@ -4,7 +4,7 @@ from comfy.compliance import medium
 @medium(
       name='rule_1_1_ensure_device_is_running_current_junos_software',
       platform=['juniper'],
-      commands=dict(chk_cmd='')
+      commands=dict(chk_cmd='show version | match JUNOS')
 )
 def rule_1_1_ensure_device_is_running_current_junos_software(commands, ref):
-    assert '' in commands.chk_cmd, ref
+    assert '15.1X49-D150.2' in commands.chk_cmd, ref
