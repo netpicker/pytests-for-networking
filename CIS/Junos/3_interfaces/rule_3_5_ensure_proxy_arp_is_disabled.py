cc5df31fc4963ac404985d9bc08a772babc95806
mailsanjayhere <mailsanjayhere@gmail.com>
2024-07-11 00:24:01 +0530

    added couple of tests

--- CIS/Junos/3_interfaces/rule_3_5_ensure_proxy_arp_is_disabled.py
+++ CIS/Junos/3_interfaces/rule_3_5_ensure_proxy_arp_is_disabled.py
@@ -4,7 +4,7 @@ from comfy.compliance import low
 @low(
       name='rule_3_5_ensure_proxy_arp_is_disabled',
       platform=['juniper'],
-      commands=dict(chk_cmd='')
+      commands=dict(chk_cmd='show configuration interfaces | match "proxy-arp" | count')
 )
 def rule_3_5_ensure_proxy_arp_is_disabled(commands, ref):
-    assert '' in commands.chk_cmd, ref
+    assert commands.chk_cmd.strip() == "0", ref
