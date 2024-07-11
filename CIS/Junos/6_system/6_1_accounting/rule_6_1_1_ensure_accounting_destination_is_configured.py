6210d42ea0ad135199ce238f559cbf1d8a3aac5e
mailsanjayhere <mailsanjayhere@gmail.com>
2024-07-11 11:13:05 +0530

    611, 612.. not updated so..

--- CIS/Junos/6_system/6_1_accounting/rule_6_1_1_ensure_accounting_destination_is_configured.py
+++ CIS/Junos/6_system/6_1_accounting/rule_6_1_1_ensure_accounting_destination_is_configured.py
@@ -4,7 +4,7 @@ from comfy.compliance import medium
 @medium(
       name='rule_6_1_1_ensure_accounting_destination_is_configured',
       platform=['juniper_junos'],
-      commands=dict(chk_cmd='show configuration system accounting destination | match "server" | count')
+      commands=dict(chk_cmd1='show configuration system accounting destination | match "server" | count')
 )
 def rule_6_1_1_ensure_accounting_destination_is_configured(commands, ref):
-    assert int(commands.chk_cmd.strip()) >= 1, ref
+    assert int(commands.chk_cmd1.strip()) >= 1, ref
