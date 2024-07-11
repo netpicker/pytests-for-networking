dc0468531318d1f6d21744abedd63736d95ed948
Netpicker <156186606+netpicker@users.noreply.github.com>
2024-07-11 11:14:55 +0530

    Juniper (#80)
    * updated rule_1_1 test
    
    * fixing flake8 reported errors
    
    * fixing flake8 errors for all vendors
    
    * fixed some more flake8
    
    * added couple of tests
    
    * updating platform name juniper to juniper_junos
    
    * added 6_1_1 test
    
    * adding 6_1_2
    
    * 611, 612.. not updated so..
    
    ---------
    
    Co-authored-by: mailsanjayhere <mailsanjayhere@gmail.com>

--- CIS/Junos/6_system/6_1_accounting/rule_6_1_2_ensure_accounting_of_logins.py
+++ CIS/Junos/6_system/6_1_accounting/rule_6_1_2_ensure_accounting_of_logins.py
@@ -4,7 +4,7 @@ from comfy.compliance import medium
 @medium(
       name='rule_6_1_2_ensure_accounting_of_logins',
       platform=['juniper_junos'],
-      commands=dict(chk_cmd='')
+      commands=dict(chk_cmd1='show configuration system accounting')
 )
 def rule_6_1_2_ensure_accounting_of_logins(commands, ref):
-    assert '' in commands.chk_cmd, ref
+    assert 'events login;' in commands.chk_cmd1, ref
