71069bd8706012f9c10bb2a89de3610fa0ec372d
mailsanjayhere <mailsanjayhere@gmail.com>
2025-09-04 19:04:06 +0530

    fiixng ios to iox.. typo

--- CVEasy/Cisco/2022/cve202220723.py
+++ CVEasy/Cisco/2022/cve202220723.py
@@ -6,29 +6,29 @@ from comfy import high
     platform=['cisco_ios'],
     commands=dict(
         show_version='show version',
-        check_ios='show running-config | include ios'
+        check_iox='show running-config | include iox'
     ),
 )
 def rule_cve202220723(configuration, commands, device, devices):
     """
     This rule checks for the CVE-2022-20723 vulnerability in Cisco IOS Software.
-    The vulnerability is due to insufficient protection in the Cisco IOS application hosting environment.
+    The vulnerability is due to insufficient protection in the Cisco IOx application hosting environment.
     Multiple vulnerabilities could allow an attacker to inject arbitrary commands into the underlying host
     operating system, execute arbitrary code, install applications without authentication, or conduct
     cross-site scripting (XSS) attacks.
     """
     # Extract the output of the command to check IOS configuration
-    ios_output = commands.check_ios
+    iox_output = commands.check_iox
 
     # Check if IOS is configured
-    ios_configured = 'ios' in ios_output
+    iox_configured = 'iox' in iox_output
 
     # Assert that the device is not vulnerable
     assert not ios_configured, (
         f"Device {device.name} is vulnerable to CVE-2022-20723. "
-        "The device has IOS application hosting configured, "
+        "The device has IOx application hosting configured, "
         "which could allow an attacker to execute arbitrary commands, install unauthorized applications, "
         "or conduct XSS attacks. "
         "For more information, see "
-        "https://tools.cisco.com/security/center/content/CiscoSecurityAdvisory/cisco-sa-ios-yuXQ6hFj"
+        "https://tools.cisco.com/security/center/content/CiscoSecurityAdvisory/cisco-sa-iox-yuXQ6hFj"
     )
